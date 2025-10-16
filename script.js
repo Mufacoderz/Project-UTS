e5ada7eb4d765af40a2634e861c019f01c6fc0f6
Muhammad <mf9555759@gmail.com>
2025-10-16 10:02:59 +0800

    reafctoring hamburger

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const btn = document.querySelector('.hamburger-btn');
 const nav = document.querySelector('.nav-list')
+const open = document.querySelector('.open');
+const close = document.querySelector('.close')
 
-btn.addEventListener('click', ()=>{
+
+//fungsi utk bar nav mobile
+function handlerHamburger(){
     nav.classList.toggle('active')
-
     if(nav.classList.contains('active')){
-        btn.style.display = 'none'
+        open.style.display = 'none'
+        close.style.display = 'block'
     }else{
-        btn.style.display = 'block'
+        open.style.display = 'block'
+        close.style.display = 'none'
     }
-})
+}
+
+//event listener utk bar nav mobile
+open.addEventListener('click', handlerHamburger)
+close.addEventListener('click', handlerHamburger)
